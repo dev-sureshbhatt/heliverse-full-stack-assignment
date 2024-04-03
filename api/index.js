13299b5c374af9e058d49f4093c4d4a8dc2f582c
dev-sureshbhatt <dev.sureshbhatt@gmail.com>
2024-04-03 08:57:45 +0530

    migration from using a JSON file for dummy data to fetching data from the database

--- api/index.js
+++ api/index.js
@@ -1,70 +1,70 @@
-import express from 'express'
-import dummyData from '../heliverse_mock_data.json' assert {type: 'json'}  
-import mongoose from 'mongoose'
-import cors from 'cors'
-import dotenv from 'dotenv'
-dotenv.config()
-import { USER } from './models/userProfile.model.js'
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+import { USER } from "./models/userProfile.model.js";
 
+const PORT = process.env.PORT | 4000;
+const app = express();
 
+app.use(cors());
 
-
-
-const PORT = process.env.PORT | 4000
-const app = express()
-
-
-
-app.use(cors())
-
-
-app.listen(PORT, ()=>{
-    console.log("App listening at PORT: ", PORT)
-})
+app.listen(PORT, () => {
+  console.log("App listening at PORT: ", PORT);
+});
 
 //Mongoose Connection
 
-
-mongoose.connect(process.env.MONGO).then(()=>{console.log("mongoose connected")}).catch((err)=>{console.log(err, "something went wrong connecting MONGOOSE")})
-
-
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("mongoose connected");
+  })
+  .catch((err) => {
+    console.log(err, "something went wrong connecting MONGOOSE");
+  });
 
 
 
 //GET all users with pagination support
-app.get('/api/users', (req,res)=>{
-    //extracting info from URL query string
-    const page = parseInt(req.query.page)
-    const limit = parseInt(req.query.limit)
+app.get("/api/users", async (req, res) => {
+  //extracting info from URL query string
+    const page = parseInt(req.query.page);
+  const limit = parseInt(req.query.limit);
 
-    
-    //slicing results that needs to be send back to the client based on query string values
-    const startIndex = (page -1) * limit
-    const endIndex = page * limit
-    const result = dummyData.slice(startIndex, endIndex)
-    
-    
-    // logic to send extra information of next and prev page 
-    const results = {}
-    
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
 
-    if (endIndex < dummyData.length){
+  const totalUsers = await USER.countDocuments();
+  console.log(totalUsers);
+
+  const results = {};
+  
+
+  if (startIndex > 0) {
+    results.prev = {
+      page: page - 1,
+      limit: limit,
+    };
+  }
+  if (endIndex < totalUsers) {
     results.next = {
-        page: page + 1,
-        limit: limit
-    }
-}
+      page: page + 1,
+      limit: limit,
+    };
+  }
 
-    if (startIndex > 0){
-        results.prev = {
-            page: page - 1,
-            limit: limit
-        }
-    }
-    
+  const fetchUsers = await USER.find().skip(startIndex).limit(limit);
+        results.result = fetchUsers;
 
-    results.result = result
-    
-    
-    res.json(results)
-})
+        res.json(results);
+});
+
+
+
+
+
+
+
+
