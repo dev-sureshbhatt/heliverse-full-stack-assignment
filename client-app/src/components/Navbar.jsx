15266530198d0cd416ffe4cef5aea028a409db57
dev-sureshbhatt <dev.sureshbhatt@gmail.com>
2024-04-03 21:13:41 +0530

    UI changes for responsiveness

--- client-app/src/components/Navbar.jsx
+++ client-app/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux'
 function Navbar() {
   const teamMembers = useSelector(state => state.team.length)
   return (
-    <div className='sticky top-0 bg-[#3B3E47] shadow-2xl flex justify-between mt-10 mb-8 p-5 w-full'>
+    <div className='sticky top-0 bg-[#3B3E47] shadow-2xl flex flex-wrap items-center justify-center sm:justify-between mt-10 mb-8 p-5 gap-5'>
         <a className='sm:text-xl text-sm' href={'/'}>Heliverse Technologies</a>
         <div className='flex gap-7 items-center'>
           {/* using a tag below to refresh the page - so that the page fetches all results again irrespective of the filter */}
@@ -14,10 +14,11 @@ function Navbar() {
             <span className='sm:text-lg text-[#738FFF] font-extrabold text-sm'>{`My Team: ${teamMembers}`}</span>
             </Link>
             <Link to={'/'}>
-            <button className='sm:text-sm'>Add More Members</button>
+            <button className='text-sm'>Add More Members</button>
             </Link>
         </div>
     </div>
+    
   )
 }
 
